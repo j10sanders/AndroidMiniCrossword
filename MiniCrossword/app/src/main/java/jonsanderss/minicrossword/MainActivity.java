ccf4c9b16325660256ef4f0bbacea53796e17df4
j10sanders <jonsandersss@gmail.com>
2016-10-12 20:02:08 -0400

    takes up full screen and back button works

--- MiniCrossword/app/src/main/java/jonsanderss/minicrossword/MainActivity.java
+++ MiniCrossword/app/src/main/java/jonsanderss/minicrossword/MainActivity.java
@@ -1,13 +1,20 @@
 package jonsanderss.minicrossword;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView mywebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = mywebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mywebView.loadUrl("http://salty-brook-41648.herokuapp.com/");
+        mywebView.setWebViewClient(new WebViewClient());
+
     }
-}
+
+    @Override
+    public void onBackPressed() {
+        if (mywebView.canGoBack()) {
+            mywebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+            return true;
+        }
+
+    }
+
+
